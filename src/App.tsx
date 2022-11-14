26ed67d6c314bc6d46adc8653f837231105d54da
Antonio Fernandes <imbitubadigital@gmail.com>
2022-11-14 08:04:11 -0300

    chore: add typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 export default function App() {
   return (
     <View style={styles.container}>
@@ -11,11 +18,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
